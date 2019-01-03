ca57478520d63baf59fbe555dce4bd336e316af5
Derpessor96 <ivan.tonchev96@gmail.com>
2019-01-03 02:35:28 +0200

    Now distingiushing between GET and POST

--- com/fmi/mpr/hw/http/HttpServer.java
+++ com/fmi/mpr/hw/http/HttpServer.java
@@ -1,6 +1,9 @@
 package com.fmi.mpr.hw.http;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -18,10 +21,34 @@ public class HttpServer {
 				System.out.println("Server attempting to listen...");
 
 				try (Socket socket = server.accept()) {
-					System.out.println("Recieved connection from " + socket.getLocalAddress());
+					System.out.println("Received connection from " + socket.getLocalAddress());
+					
+					process(socket);
 				}
 			} catch (Exception e) {
-				System.out.println("Error while recieving connection!");
+				System.out.println("Error while receiving connection!");
+			}
+		}
+	}
+
+	private void process(Socket socket) throws IOException {
+		try(InputStream socketStream = socket.getInputStream();
+				InputStreamReader inputReader = new InputStreamReader(socketStream);
+				BufferedReader bufferedReader = new BufferedReader(inputReader)) {
+			String firstLine = bufferedReader.readLine();
+			
+			
+			if(firstLine == null) {
+				System.out.println("Received empty request!");
+			}
+			else if(firstLine.startsWith("POST")) {
+				System.out.println("Received POST request!");
+			}
+			else if(firstLine.startsWith("GET")) {
+				System.out.println("Received GET request!");
+			}
+			else {
+				System.out.println("Unknown request type!");
 			}
 		}
 	}
--- com/fmi/mpr/hw/http/Main.java
+++ com/fmi/mpr/hw/http/Main.java
@@ -1,27 +1,27 @@
 package com.fmi.mpr.hw.http;
 
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
 		HttpServer server;
-		
-		while(true) {
-			System.out.print("Please enter server port: ");
-			
-			int port = scanner.nextInt();
-			
-			try {
-				server = new HttpServer(port);
-				
-				break;
-			} catch (Exception e) {
-				System.out.println("Could not initialize server");
+
+		try (Scanner scanner = new Scanner(System.in)) {
+			while (true) {
+				System.out.print("Please enter server port: ");
+
+				int port = scanner.nextInt();
+
+				try {
+					server = new HttpServer(port);
+
+					break;
+				} catch (Exception e) {
+					System.out.println("Could not initialize server");
+				}
 			}
 		}
-		
+
 		server.listen();
 	}
 }
