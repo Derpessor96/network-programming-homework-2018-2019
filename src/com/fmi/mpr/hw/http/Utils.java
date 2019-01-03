9bdabc97dfb0f430a216cf1058e26a77ac35ec19
Derpessor96 <ivan.tonchev96@gmail.com>
2019-01-03 03:38:11 +0200

    Added utility method for extracting headers from a request

--- src/com/fmi/mpr/hw/http/Utils.java
+++ src/com/fmi/mpr/hw/http/Utils.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 public final class Utils {
 	private Utils() {
@@ -17,16 +19,48 @@ public final class Utils {
 				InputStreamReader inputReader = new InputStreamReader(input);
 				BufferedReader bufferedInput = new BufferedReader(inputReader)) {
 			StringBuilder builder = new StringBuilder();
-			String currLine = "";
+			String currLine;
 			
-			while(currLine != null) {
+			do {
 				currLine = bufferedInput.readLine();
+				if(currLine == null) {
+					break;
+				}
+				
 				builder.append(currLine);
+				builder.append("\r\n");
 			}
+			while(true);
 			
 			result = builder.toString();
 		}
 		
 		return result;
+	}
+	
+	public static Map<String, String> getHeaders(BufferedReader reader) throws IOException {
+		Map<String, String> headers = new HashMap<>();
+		String currLine;
+		
+		do {
+			currLine = reader.readLine();
+			if(currLine == null || currLine.isEmpty()) {
+				break;
+			}
+
+			int splitIndex = currLine.indexOf(":");
+			
+			if(splitIndex == -1) {
+				continue;
+			}
+			
+			String key = currLine.substring(0, splitIndex);
+			String value = currLine.substring(splitIndex);
+			
+			headers.put(key, value);
+		}
+		while(true);
+			
+		return headers;
 	}
 }
